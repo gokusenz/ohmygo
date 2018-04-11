751b66afa644fe11c615bffc7209c5073ad61947
pallat <yod.pallat@gmail.com>
2018-04-11 17:51:50 +0700

    multiple value with testing

---
+++ multiple.go
@@ -0,0 +1,5 @@
+package oh_my_go
+
+func forward(a, b int) (int, int) {
+	return 0, 0
+}
---
+++ multiple_test.go
@@ -0,0 +1,27 @@
+package oh_my_go
+
+import "testing"
+
+func TestMultipleReturnValues1(t *testing.T) {
+	a, b := forward(1, 2)
+
+	if a != 2 && b != 3 {
+		t.Error("it should return right value and sum of them,", a, b)
+	}
+}
+
+func TestMultipleReturnValues2(t *testing.T) {
+	a, b := forward(2, 3)
+
+	if a != 3 && b != 5 {
+		t.Error("it should return right value and sum of them,", a, b)
+	}
+}
+
+func TestMultipleReturnValues3(t *testing.T) {
+	a, b := forward(3, 5)
+
+	if a != 5 && b != 8 {
+		t.Error("it should return right value and sum of them,", a, b)
+	}
+}
