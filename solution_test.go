0e20512c010120f49efa1fb5217396c130351615
pallat <yod.pallat@gmail.com>
2018-05-29 14:08:04 +0700

    more test

--- solution_test.go
+++ solution_test.go
@@ -10,3 +10,31 @@ func TestFizzBuzz1(t *testing.T) {
 		t.Error("1 should be 1 but got", result)
 	}
 }
+
+func TestFizzBuzz2(t *testing.T) {
+	result := fizzbuzz(2)
+	if result != "2" {
+		t.Error("2 should be 2 but got", result)
+	}
+}
+
+func TestFizzBuzz3(t *testing.T) {
+	result := fizzbuzz(3)
+	if result != "Fizz" {
+		t.Error("3 should be Fizz but got", result)
+	}
+}
+
+func TestFizzBuzz4(t *testing.T) {
+	result := fizzbuzz(4)
+	if result != "4" {
+		t.Error("4 should be 4 but got", result)
+	}
+}
+
+func TestFizzBuzz5(t *testing.T) {
+	result := fizzbuzz(5)
+	if result != "Buzz" {
+		t.Error("5 should be Buzz but got", result)
+	}
+}
