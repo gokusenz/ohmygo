e7a6185a89a44fb727472cf51d852cad0b60be9f
pallat <yod.pallat@gmail.com>
2018-05-29 13:59:53 +0700

    more test

--- solution_test.go
+++ solution_test.go
@@ -10,3 +10,24 @@ func TestFactorial3(t *testing.T) {
 		t.Error("factorial of 3 is 6 but you've got", result)
 	}
 }
+
+func TestFactorial4(t *testing.T) {
+	result := factorial(4)
+	if result != 24 {
+		t.Error("factorial of 4 is 24 but you've got", result)
+	}
+}
+
+func TestFactorial5(t *testing.T) {
+	result := factorial(5)
+	if result != 120 {
+		t.Error("factorial of 5 is 120 but you've got", result)
+	}
+}
+
+func TestFactorial6(t *testing.T) {
+	result := factorial(6)
+	if result != 720 {
+		t.Error("factorial of 6 is 720 but you've got", result)
+	}
+}
